7be2878f85ede1d96e2f4298ef7fa027c7351621
Lukixs <zeblukasz@gmail.com>
2021-12-02 13:06:27 +0100

    [Feature] Improved Online Rooms system (#1)
    * Added static rooms
    
    * Adaptation to new component system of online game

--- index.js
+++ index.js
@@ -3,62 +3,64 @@ const io = require("socket.io")(http, {
   cors: { origin: "*" },
 });
 
-let clientNo = 0;
-let clientName;
+let rooms = [
+  { id: "room1", name: "Pokój 1", players: [] },
+  { id: "room2", name: "Pokój 2", players: [] },
+  { id: "room3", name: "Pokój 3", players: [] },
+  { id: "room4", name: "Pokój 4", players: [] },
+  { id: "room5", name: "Pokój 5", players: [] },
+  { id: "room6", name: "Pokój 6", players: [] },
+];
+let playerColor;
 
 io.on("connection", (socket) => {
-  clientNo++;
-  const currentClientNo = clientNo;
-  const currentRoom = Math.round(currentClientNo / 2);
-  console.log(
-    `user ${socket.id.substring(0, 3)} connected to room: ${currentRoom}`
-  );
+  let currentRoomId;
+  let clientName;
 
-  socket.on("joinRoom", (name) => {
-    if (name == "" || !name) clientName = `player ${socket.id.substring(0, 3)}`;
-    else clientName = name;
-    socket.join(currentRoom);
-    // console.log("socket joined " + currentRoom);
-    const playerMoveOnWhite = currentClientNo % 2 == 1;
-    socket.emit("joined", playerMoveOnWhite);
+  socket.emit("rooms", rooms);
+
+  socket.on("joinRoom", (playerName, roomName) => {
+    if (playerName == "" || !playerName)
+      clientName = `player ${socket.id.substring(0, 3)}`;
+    else clientName = playerName;
+
+    const currentPlayer = { name: clientName, id: socket.id };
+    const currentRoom = rooms.find((room) => room.name == roomName);
+    if (!currentRoom || currentRoom.players.length >= 2) return;
+    socket.join(currentRoom.id);
+    currentRoom.players.push(currentPlayer);
+    currentRoomId = currentRoom.id;
+
+    playerColor = currentRoom.players.length % 2 == 1;
+    socket.emit("joined");
+    socket.broadcast.emit("rooms", rooms);
   });
 
   socket.on("message", (message) => {
-    console.log(message);
-    // io.emit(
-    //   "message",
-    //   `${socket.id.substr(0, 5)} said ${message}, ${currentRoom} `
-    // );
+    if (currentRoomId === "") return;
 
-    if (currentRoom === "") {
-      socket.broadcast.emit("message", `${message} to everyone`);
-      socket.emit("message", `${message} to room ${currentRoom} `);
-    } else {
-      socket
-        .to(currentRoom)
-        .emit("message", clientName, `${message} to room ${currentRoom} `);
-      socket.emit("message", clientName, `${message} to room ${currentRoom} `);
-    }
+    socket.to(currentRoomId).emit("message", clientName, message);
+    socket.emit("message", clientName, message);
   });
 
   socket.on("place-pawn", (pawn) => {
-    // console.log(pawn);
-    if (currentRoom != "") socket.to(currentRoom).emit("place-pawn", pawn);
+    if (currentRoomId != "") socket.to(currentRoomId).emit("place-pawn", pawn);
   });
 
   socket.on("move-pawn", (pawn) => {
-    // console.log(pawn);
-    if (currentRoom != "") socket.to(currentRoom).emit("move-pawn", pawn);
+    if (currentRoomId != "") socket.to(currentRoomId).emit("move-pawn", pawn);
   });
 
   socket.on("pawn-scored", (pawn) => {
-    // console.log(pawn);
-    if (currentRoom != "") socket.to(currentRoom).emit("pawn-scored", pawn);
+    if (currentRoomId != "") socket.to(currentRoomId).emit("pawn-scored", pawn);
   });
 
   socket.on("remove-pawn", (pawn) => {
-    // console.log(pawn);
-    if (currentRoom != "") socket.to(currentRoom).emit("remove-pawn", pawn);
+    if (currentRoomId != "") socket.to(currentRoomId).emit("remove-pawn", pawn);
+  });
+
+  socket.on("get-player-color", () => {
+    socket.emit("player-color", playerColor);
   });
 });
 
